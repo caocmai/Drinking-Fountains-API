932ad3498219ba58037eb9725a3e2f31d17e42dd
Cao Mai <caomai@rocketmail.com>
2020-03-05 13:48:07 -0800

    [update] auth tests to pass

--- src/test/auth.js
+++ src/test/auth.js
@@ -1,12 +1,18 @@
 const chai = require("chai");
 const chaiHttp = require("chai-http");
-const server = require("../server");
+const app = require("../server");
 const should = chai.should();
 chai.use(chaiHttp);
 
-const agent = chai.request.agent(server);
+const agent = chai.request.agent(app);
+const assert = chai.assert;
 
 const User = require("../models/user");
+
+sampleUser = {
+  username: 'testuser1',
+  password: 'somepassword'
+}
 
 describe("User", function() {
 
@@ -21,19 +27,41 @@ describe("User", function() {
       });
   });
 
-  Signup
-  it("should be able to signup", function(done) {
-    User.findOneAndRemove({ username: "testone" }, function() {
-      agent
-        .post("/user/signup")
-        .send({ username: "testone", password: "password" })
-        .end(function(err, res) {
-          res.should.have.status(200);
-          agent.should.have.cookie("pToken");
-          done();
-        });
-    });
-  });
+  // Signup
+  it('should be able to sign up', (done) => {
+    chai.request(app)
+      .post('/user/signup')
+      .send(sampleUser)
+      .then(res => {
+        assert.equal(res.status, 200)
+        assert.exists(res.body.jwtToken)
+
+        User.find({username: 'testuser1'}).then(result => {
+          assert.equal(result.length, 1)
+        })
+        return done()
+      }).catch(err => {
+        return done(err)
+      })
+  })
+
+  it('should be able to log in', (done) => {
+    let user = new User(sampleUser)
+    user.save().then(savedUser => {
+      chai.request(app)
+        .post('/user/login')
+        .send(sampleUser)
+        .then(res => {
+          console.log(res.body)
+          assert.equal(res.status, 200)
+          assert.exists(res.body.jwtToken)
+          return done()
+        }).catch(err => {
+          console.log(err)
+          return done(err)
+        })
+    })
+  })
 
   // login
   // it("should be able to login", function(done) {
